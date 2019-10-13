8bd33e1b993d33f633348909d54d78650c2dd72d
shaka0241 <shaka0241@gmail.com>
2019-10-13 00:03:42 -0300

    conectado api accuweather / informacion basica de santiago-paris-tokyo

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,28 @@
 <template>
   <div id="app">
+    <HelloWorld></HelloWorld>
     <b-container class="bv-example-row">
-      <b-row>
-        <b-col>1 of 3</b-col>
-        <b-col>2 of 3</b-col>
-        <b-col>3 of 3</b-col>
-      </b-row>
-  </b-container>
+  <b-row>
+    <b-col>
+      <ul v-for="santiago in santiagos" v-bind:key="santiagos.id">
+      <li>{{santiago}}</li>
+      </ul>
+      <button @click="fetchSantiago">Informacion Actual Santiago</button>
+    </b-col>
+    <b-col>
+      <ul v-for="paris in pariss" v-bind:key="pariss.id">
+      <li>{{paris}}</li>
+      </ul>
+      <button @click="fetchParis">Informacion Actual Paris</button>
+    </b-col>
+    <b-col>
+      <ul v-for="tokyo in tokyos" v-bind:key="tokyos.id">
+      <li>{{tokyo}}</li>
+      </ul>
+      <button @click="fetchTokyo">Informacion Actual tokyo</button>
+    </b-col>
+  </b-row>
+</b-container>
   </div>
 </template>
 
@@ -15,8 +31,38 @@ import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
+    data () {
+    return {
+      santiagos: [],
+      pariss:[],
+      tokyos:[]
+    }
+  },
   components: {
     HelloWorld
+  },
+  methods: {
+    fetchSantiago: function () {
+      const baseURI = 'http://dataservice.accuweather.com/forecasts/v1/daily/1day/60449?apikey=SPDhK9W0OG4AGinAcZGn9t5gRqxg7cnA&language=es-cl'
+      this.$http.get(baseURI)
+      .then((result) => {
+        this.santiagos = result.data
+      })
+    },
+    fetchParis: function () {
+      const baseURI = 'http://dataservice.accuweather.com/forecasts/v1/daily/1day/133785?apikey=SPDhK9W0OG4AGinAcZGn9t5gRqxg7cnA&language=es-cl'
+      this.$http.get(baseURI)
+      .then((result) => {
+        this.santiagos = result.data
+      })
+    },
+    fetchTokyo: function () {
+      const baseURI = 'http://dataservice.accuweather.com/forecasts/v1/daily/1day/133785?apikey=SPDhK9W0OG4AGinAcZGn9t5gRqxg7cnA&language=es-cl'
+      this.$http.get(baseURI)
+      .then((result) => {
+        this.santiagos = result.data
+      })
+    },
   }
 }
 </script>
